b1f2e8de6451ef5cca486f842328036bdc1df14e
ervinay-2112 <78842505+ervinay-2112@users.noreply.github.com>
2022-02-24 11:09:52 +0530

    new projects wrok

---
+++ Tavus-project/assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        navigation : true, // Show next and prev buttons
+   
+        slideSpeed : 3000,
+        paginationSpeed : 400,
+        autoplay: true,
+        dots: true,
+        items : 1, 
+        itemsDesktop : false,
+        loop:true
+   
+    });
+   
+  });
